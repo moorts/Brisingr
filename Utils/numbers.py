0827132263802c0795d22782bf81fdc685741cfc
moorts <moritz.gutfleisch@gmx.de>
2021-11-03 17:20:54 +0100

    Whatever

---
+++ Utils/numbers.py
@@ -0,0 +1,33 @@
+def convergents(e):
+    n = [] # Nominators
+    d = [] # Denominators
+
+    for i in range(len(e)):
+        if i == 0:
+            ni = e[i]
+            di = 1
+        elif i == 1:
+            ni = e[i]*e[i-1] + 1
+            di = e[i]
+        else: # i > 1
+            ni = e[i]*n[i-1] + n[i-2]
+            di = e[i]*d[i-1] + d[i-2]
+
+        n.append(ni)
+        d.append(di)
+        yield (ni, di)
+
+def cf_expansion(n, d):
+    e = []
+
+    q = n // d
+    r = n % d
+    e.append(q)
+
+    while r != 0:
+        n, d = d, r
+        q = n // d
+        r = n % d
+        e.append(q)
+
+    return e
